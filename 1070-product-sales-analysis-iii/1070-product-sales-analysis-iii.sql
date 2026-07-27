5793ba1f9ac7716734266860862f44650a2b5bfe
Chitrak Betal <chitrak14789@gmail.com>
2026-07-28 05:22:32 +0530

    Time: 841 ms (21.6%), Space: 0B (100%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,15 @@
+SELECT
+s.product_id,
+t.first_year,
+s.quantity,
+s.price
+FROM Sales as s
+JOIN(
+    SELECT
+        product_id,
+        MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) AS t
+ON s.product_id = t.product_id
+WHERE s.year = t.first_year
