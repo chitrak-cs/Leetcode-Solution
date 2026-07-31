b8416184ff184a7f75c27bc0d2cfbe117b417c6d
Chitrak Betal <chitrak14789@gmail.com>
2026-07-31 15:10:02 +0530

    Time: 0 ms (100%), Space: 9.4 MB (11.24%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+       unordered_map<char,int>mpp;
+        int n = word.size();
+        int click = 0;
+
+        for(auto letter:word){
+            mpp[letter]++;
+        }
+
+        vector<int>freq;
+        for(auto it:mpp){
+            int f = it.second;
+            freq.push_back(f);
+        }
+
+        sort(freq.begin(),freq.end());
+        reverse(freq.begin(),freq.end());
+
+        for(int i=0;i<freq.size();i++){
+            if(i<=7){
+                click += freq[i];
+            }
+            else if(i <= 15){
+                click += 2 * freq[i];
+            }
+            else if(i <= 23){
+                click += 3*freq[i];
+            }
+            else{
+                click += 4*freq[i];
+            }
+        }
+
+        return click; 
+    }
+};
