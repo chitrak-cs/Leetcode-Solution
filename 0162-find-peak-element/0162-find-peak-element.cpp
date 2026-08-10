e8355465f7f420d5ad4a89f0dfccc542df741fcb
Chitrak Betal <chitrak14789@gmail.com>
2026-08-10 18:40:01 +0530

    Time: 0 ms (100%), Space: 12.8 MB (6.84%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -2,14 +2,18 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        if(n==1) return 0;
+        int low = 0;
+        int high = n-1;
 
-        if(nums[0] > nums[1]) return 0;
-        if(nums[n-1] > nums[n-2]) return n-1;
-
-        for(int i=1;i<n-1;i++){
-            if(nums[i] > nums[i-1] && nums[i] > nums[i+1]) return i;
+        while(low < high){
+            int mid = low + (high-low)/2;
+            if(nums[mid] > nums[mid+1]){
+                high = mid;
+            }
+            else{
+                low = mid+1;
+            }
         }
-        return -1;
+        return low;
     }
 };
