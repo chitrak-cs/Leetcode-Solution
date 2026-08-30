2f135c05e4a112e41101e0b213b90534d10d34db
Chitrak Betal <chitrak14789@gmail.com>
2026-08-30 09:13:00 +0530

    Time: 0 ms (100%), Space: 24.3 MB (14.29%) - LeetHub

---
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        vector<long long> rem(24, 0);
+
+        for (auto h : hours) {
+            rem[h % 24]++;
+        }
+
+        long long ans = 0;
+
+        int f0 = rem[0];
+        ans +=1LL * f0 * (f0 - 1) / 2;
+
+        int f12 = rem[12];
+        ans +=  1LL * f12 * (f12 - 1) / 2;
+
+        for (int i = 1; i < 12; i++) {
+            ans += 1LL * rem[i] * rem[24 - i];
+        }
+
+        return ans;
+    }
+};
